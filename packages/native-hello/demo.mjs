440009a228df47fb071e0851985f7ae121f56764
sakompella <87831937+sakompella@users.noreply.github.com>
2026-05-11 18:33:45 -0700

    toss test rust stuff

--- packages/native-hello/demo.mjs
+++
@@ -1,6 +0,0 @@
-import { createRequire } from "node:module";
-
-const require = createRequire(import.meta.url);
-const { hello } = require("./index.js");
-
-console.log(hello());
--- packages/native-hello/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use napi_derive::napi;
-
-#[napi]
-pub fn hello() -> String {
-    String::from("hello from native-hello")
-}
